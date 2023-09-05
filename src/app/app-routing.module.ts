580fca8a98b51e80a7eda9bbc4275b71adba11f5
Bernardo Fusco <blfuscomail@gmail.com>
2023-09-05 13:11:22 -0300

    Commit dev-angular (commit on 'Master')

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Exemplo1Component } from './exemplo1/exemplo1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'exemplo1', component: Exemplo1Component },
+  { path: '', redirectTo: '/exemplo1', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PessoaService } from './pessoa.service';
+import { TabPessoaComponent } from './tab-pessoa/tab-pessoa.component';
+import { ListPessoaComponent } from './list-pessoa/list-pessoa.component';
+import { PanelPessoaComponent } from './panel-pessoa/panel-pessoa.component';
+import { Exemplo1Component } from './exemplo1/exemplo1.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TabPessoaComponent, ListPessoaComponent, PanelPessoaComponent, Exemplo1Component],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [PessoaService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
